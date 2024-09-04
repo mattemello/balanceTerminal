e8ab0ba0b86e54ef58a56f495a63353d7e014cc2
matteo <matteodragonfire2312@gmail.com>
2024-09-04 18:37:47 +0200

    implemant of sqlite, a lot to do

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/mattemello/balanceTerminal/ui"
+	"github.com/mattn/go-sqlite3"
 )
 
 func main() {
 
+	db, err := sql.Open("sqlite3", "money.db")
+	if err != nil {
+		log.Fatal("Error with the database: ", err)
+		return
+	}
+
+	queryCre := "CREAT "
+
 	app := ui.AppCreation()
 	pages := ui.PageCreation()
-
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Rune() == 113 {
 			app.Stop()
--- ui/pages.go
+++ ui/pages.go
@@ -17,10 +17,37 @@ func PageCreation() *tview.Pages {
 	textA := tview.NewTextView().SetText("(q) for quit").SetTextColor(tcell.ColorSnow)
 	textA.SetBackgroundColor(tcell.ColorBlack)
 
-	pages.AddPage("Menu", flexCreation().AddItem(tview.NewBox().SetBorder(true), 0, 1, false).AddItem(textA, 0, 4, false), true, true)
+	pages.AddPage("Menu", menuCreation(), true, true)
 	pages.AddPage("Insert", flexCreation().AddItem(tview.NewBox().SetBorder(true), 0, 1, false).AddItem(tview.NewBox().SetBorder(true), 0, 1, false).AddItem(tview.NewBox().SetBorder(true), 0, 1, false), true, false)
 
 	return pages
+}
+
+func menuCreation() *tview.Flex {
+	flex := flexCreation()
+
+	flex.AddItem(tview.NewBox().SetBorder(true), 0, 2, false)
+	flex.AddItem(tview.NewBox().SetBorder(true), 0, 7, false)
+
+	flex.AddItem(footSet(), 0, 1, false)
+
+	return flex
+}
+
+func footSet() *tview.Flex {
+
+	text := tview.NewTextView().SetText("(n) new pay \t (d) delet").SetTextColor(tcell.ColorSnow)
+	text.SetTextAlign(tview.AlignBottom)
+	text.SetTextAlign(tview.AlignCenter)
+
+	text1 := tview.NewTextView().SetText("(q) quit").SetTextColor(tcell.ColorSnow)
+	text1.SetTextAlign(tview.AlignBottom)
+
+	keyboard := tview.NewFlex()
+	keyboard.AddItem(text, 0, 10, false).AddItem(text1, 0, 1, false)
+	keyboard.SetBorder(true)
+
+	return keyboard
 }
 
 /*
