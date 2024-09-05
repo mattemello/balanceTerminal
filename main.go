d39b6cad19a1c316a9d286d340a6b5ee11ac07cd
matteo <matteodragonfire2312@gmail.com>
2024-09-05 17:30:34 +0200

    understand of sqlite with go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/mattemello/balanceTerminal/ui"
-	"github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -17,7 +17,24 @@ func main() {
 		return
 	}
 
-	queryCre := "CREAT "
+	queryCre := "CREATE TABLE IF NOT EXISTS spendingMoney( id_transition INTEGER PRIMARY KEY, valur_tansaction REAL, tags TEXT, date DATE )"
+
+	if _, err := db.Exec(queryCre); err != nil {
+		log.Fatal("Error with the database: ", err)
+		return
+	}
+
+	/*_, err = db.Exec("INSERT INTO spendingMoney VALUES(0, 0.5, 'real', 05/12/2021);")
+	if err != nil {
+		log.Fatal("29 Error with the database: ", err)
+		return
+	}
+
+	_, err = db.Exec("DELETE FROM spendingMoney WHERE id_transition=0;")
+	if err != nil {
+		log.Fatal("Error with the database: ", err)
+		return
+	}*/
 
 	app := ui.AppCreation()
 	pages := ui.PageCreation()
